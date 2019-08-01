98ee009a53b08ec318104034c313ba32081bbd49
FPT Aptech Nguyễn Thành Đạt <datntd00498@fpt.edu.vn>
2019-08-01 11:40:24 +0700

    chaclaxong

--- src/main/java/helper/ObjectifyHelper.java
+++ src/main/java/helper/ObjectifyHelper.java
@@ -1,13 +1,28 @@
 package helper;
 
 import com.googlecode.objectify.ObjectifyService;
+import entity.Employee;
 
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
+import javax.servlet.*;
+import java.io.IOException;
 
-public class ObjectifyHelper implements ServletContextListener {
+
+public class ObjectifyHelper implements Filter {
+
+
     @Override
-    public void contextInitialized(ServletContextEvent sce) {
-        ObjectifyService
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        ObjectifyService.register(Employee.class);
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
     }
 }
